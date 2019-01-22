e25817c14032d613aff10a32a21eb126df4f0a45
Power1234 <2445182354@qq.com>
2019-01-22 18:33:30 +0800

    spring-cloud-demo

---
+++ src/main/java/com/happok/demo/user/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.happok.demo.user.controller;
+
+/**
+ * @ClassName UserController
+ * @Description
+ * @Author Administrator
+ * @Date 2019/1/22 0022 下午 18:30
+ * @Version 1.0
+ */
+public class UserController {
+}
